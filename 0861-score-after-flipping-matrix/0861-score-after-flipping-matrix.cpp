eb3b519d88892b8b4f6781c515ce37fff0a8dbe4
Manish Saini <saini36785@gmail.com>
2026-09-18 17:30:09 +0530

    Time: 0 ms (100%), Space: 11.2 MB (94.6%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        //making the first element all ones
+        for(int i=0;i<rows ; i++) {
+            if(grid[i][0]==0) {
+                for(int j=0;j<cols; j++) {
+                    if(grid[i][j] == 0) {
+                        grid[i][j] = 1;
+                    }else{
+                        grid[i][j]=0;
+                    }
+                    
+                }
+            }
+        }
+
+        //flip the cols where noz>noO
+        for(int j= 0; j< cols; j++) {
+            int noZ =0 ;
+            int noO = 0; 
+            for(int i =0;i<rows ; i++) {
+                if(grid[i][j] ==0) {
+                    noZ++;
+
+                }else{
+                    noO++;
+                }
+            }
+            if(noZ>noO){
+                for(int i=0;i<rows;i++) {
+                    if(grid[i][j] == 0) {
+                        grid[i][j] = 1;
+                    }else{
+                        grid[i][j]=0;
+                    }
+                    
+                }
+            }    
+        }
+        int sum = 0 ;
+        for(int i= 0; i<rows ;i++) {
+            int x=1;
+            for(int j=cols- 1; j >=0; j--){
+                sum += grid[i][j]*x;
+                x = x*2;
+            }
+        }
+        return sum;
+        
+    }
+};
