ecb53f2693736fa43a386d9621e42165b7448ade
Manish Saini <saini36785@gmail.com>
2026-09-24 15:36:25 +0530

    Time: 0 ms (100%), Space: 15 MB (54.99%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int sum = 0;
-
-        int n = mat.size();
-        for(int i = 0; i<n; i++) {
+        int n  = mat.size();
+        int sum  = 0 ;
+        for(int i = 0 ;i <n;i++) {
             sum  = sum + mat[i][i];
-            sum   =sum + mat[i][n-1-i];
-        
+        }
+        for(int i = 0 ,j=n-1; i<n && j>=0; i++,j--){
+            sum = sum + mat[i][j];
         }
         if(n%2 == 1) {
-            sum = sum - mat[n/2][n/2];  //n/2 = 3/2 = 1  intiger devision 
+            sum  = sum - mat[n/2][n/2];
         }
 
-        return sum;
-        
+        return sum ;
         
     }
 };
